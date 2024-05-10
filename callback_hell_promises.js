ddb857b2405771f2346c04312f8b4c6f05130842
Caleb Pierre <calebsaunders@gmail.com>
2024-05-10 06:27:53 -0700

    modified  to use promises

--- callback_hell_promises.js
+++ callback_hell_promises.js
@@ -4,4 +4,27 @@ const fs = require("fs").promises;
 // TODO: Rewrite the nested file operations using Promise chains.
 function nestedFileOperationsPromises() {
   // Implement the promise-based version of nested file operations here.
+
+  fs.readFile("file1.txt", "utf-8")
+    .then((data1) => {
+      console.log("File 1 read successfully:", data1);
+      const nextFileName = data1.trim() + ".txt";
+
+      return fs.readFile(nextFileName, "utf-8");
+    })
+    .then((data2) => {
+      console.log("Second file read successfully:", data2);
+
+      const modifiedData = data2.toUpperCase();
+
+      return fs.writeFile("modifiedData.txt", modifiedData);
+    })
+    .then(() => {
+      console.log("Modified data written successfully");
+    })
+    .catch((err) => {
+      console.error("Error occured:", err);
+    });
 }
+
+nestedFileOperationsPromises();
